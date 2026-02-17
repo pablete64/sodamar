3c71915026ed574edce712506f5d28c07b668388
Pablo Cabaleiro Noda <pablocabaleironoda@gmail.com>
2026-02-17 15:37:40 +0000

    Fix: Add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <CookieConsent />
-      <BrowserRouter>
+      <BrowserRouter basename="/sodamar">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
